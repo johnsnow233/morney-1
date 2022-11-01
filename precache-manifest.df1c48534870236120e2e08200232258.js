955ec198f67864641488b2ab134b6c4e81cdd247
JohnSnow <54940899@qq.com>
2022-11-01 22:31:00 +0800

    deploy

---
+++ precache-manifest.df1c48534870236120e2e08200232258.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "378087ec3c6c89e0bea7",
+    "url": "/morney-1/css/app.be117603.css"
+  },
+  {
+    "revision": "d29f16b9a0796a167bd272595d5d7cb5",
+    "url": "/morney-1/index.html"
+  },
+  {
+    "revision": "378087ec3c6c89e0bea7",
+    "url": "/morney-1/js/app.4b31390f.js"
+  },
+  {
+    "revision": "cbd8e5c14983d198ea1b",
+    "url": "/morney-1/js/chunk-vendors.076fd770.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-1/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-1/robots.txt"
+  }
+]);
